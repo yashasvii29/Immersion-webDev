fe20964521c4540ec6b2f513d684a25e2fd05f5b
Yashasvi Agrawal <122177943+yashasvii29@users.noreply.github.com>
2024-08-25 00:20:03 +0530

    add variable folder

---
+++ variable/app.js
@@ -0,0 +1,22 @@
+var a=10;
+var a=20;
+a=30;
+var d;
+
+// in case of var redeclare bhi kr sakte hai and reinitialise bhi
+console.log(a);
+
+
+let b = 5;
+// let b = 10;
+b=10;
+let k;
+// let ke case m only reinitialse means reassign kr sakte hai
+console.log(b)
+
+
+// const c;  error aayega bcz const ke case m we cannot declare tge varaible without its initialisation
+const c=5;
+// const ke case m redeclare and reinitialise dono hi nhi kr sakte
+//  c=77;
+console.log(c)
