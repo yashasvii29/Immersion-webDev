29590047cc3c76e3682d39e6d12a3f51edd67d72
Yashasvi Agrawal <122177943+yashasvii29@users.noreply.github.com>
2024-08-27 22:41:18 +0530

    add code

--- this_keyword/app.js
+++ this_keyword/app.js
@@ -9,6 +9,7 @@
 // ----------------------------------------
 
 // 1.functional calling(direct) : window object
+// window is a global object
 // function fun(){
 //     console.log(this);
 // }
@@ -75,7 +76,8 @@
 //   this.naam = "shinchan";
 // }
 // let obj = new Fun();
-// console.log(obj);//Fun()
+// In constructor calling new keyword ki help se uss class ka object create krenge so in this case this always refers to newly created object
+// console.log(this);//obj()
 
 // -----------------------------------------------
 // 4. indirect calling : call , apply , bind
@@ -108,8 +110,8 @@
   
   // -----------------------------------------------
   
-  // 5. arrow function calling : points at parent's this
-  
+  // 5. arrow function calling : points at parent's this..means this refers to parents this
+  // in arrow fun calling , this keyword refers to its (parents this)
   // let obj = {
   //     a:10,
   //     b:20,
@@ -117,6 +119,7 @@
   //         console.log(this);
   //     }
   // }
+  // obj.fn();//window
   // -----------------------------------------------
   
   // let obj = {
@@ -125,9 +128,10 @@
   //     let f = () => {
   //       console.log(this);
   //     };
+  //     f();
   //   },
   // };
-  // obj.fn();
+  // obj.fn(); //window
   
   // -----------------------------------------------
   
@@ -137,8 +141,75 @@
   //     let f = () => {
   //       console.log(this);
   //     };
+  //     f();
   //   },
   // };
-  // obj.fn();
+  // obj.fn(); //obj
   
   // -----------------------------------------------
+
+
+
+
+
+
+
+
+
+
+  // call method example
+
+  let p1 = {
+    firstName: 'John',
+    lastName: 'Smith'
+  };
+  let p2 = {
+    firstName: 'Ann',
+    lastName: 'Brown'
+  };
+  function sayWelcome(greeting) {
+    console.log(greeting + ' ' + this.firstName + ' ' + this.lastName);
+  }
+  sayWelcome.call(p1, 'Welcome'); // Welcome John Smith
+  sayWelcome.call(p2, 'Welcome'); // Welcome Ann Brown
+
+
+
+
+  // apply method example
+
+
+  let p1 = {
+    firstName: 'John',
+    lastName: 'Smith'
+  };
+  let p2 = {
+    firstName: 'Ann',
+    lastName: 'Brown'
+  };
+  function sayWelcome(greeting) {
+    console.log(greeting + ' ' + this.firstName + ' ' + this.lastName);
+  }
+  sayWelcome.apply(p1, ['Welcome']); // Welcome John Smith
+  sayWelcome.apply(p2, ['Welcome']); // Welcome Ann Brown
+
+
+
+
+  // bind method example
+
+  let p1 = {
+    firstName: 'John',
+    lastName: 'Smith'
+  };
+  let p2 = {
+    firstName: 'Ann',
+    lastName: 'Brown'
+  };
+  function sayWelcome() {
+    console.log('Welcome ' + this.firstName + ' ' + this.lastName);
+  }
+  let sayWelcomeJohn = sayWelcome.bind(p1);
+  let sayWelcomeAnn = sayWelcome.bind(p2);
+  sayWelcomeJohn(); // Welcome John Smith
+  sayWelcomeAnn(); // Welcome Ann Brown
